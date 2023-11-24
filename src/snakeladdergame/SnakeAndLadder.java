def008f520bd04fb4761c4b0d3904d26b44b8dc4
Anushka <anushkamar1102@gmail.com>
2023-11-24 14:27:47 +0530

    Refactored: added the functionality to check for options

--- src/snakeladdergame/SnakeAndLadder.java
+++ src/snakeladdergame/SnakeAndLadder.java
@@ -1,6 +1,33 @@
 package snakeladdergame;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
+
+    public static int checkOptn(Player player1, int diceVal){
+
+        int currPos = player1.getCurrPos();
+        Random random = new Random();
+        int ranNum = random.nextInt(3);
+
+        switch (ranNum){
+            case 0:
+                System.out.println("Oops, you can't move, it's a NO play");
+                currPos += 0;
+                break;
+            case 1:
+                System.out.println("yay, you got on a ladder");
+                currPos += diceVal;
+                break;
+            case 2:
+                System.out.println("You got bitten by a snake");
+                if(currPos-diceVal<0){
+                    currPos = 0;
+                }else {currPos -= diceVal;}
+
+        }
+        return currPos;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to the game");
 
@@ -11,6 +38,8 @@ public class SnakeAndLadder {
         while (player1.getCurrPos()<100){
 
             int diceVal = dice.rollDice();
+            int currPos = checkOptn(player1, diceVal);
+            System.out.println("Your current position is: "+currPos);
         }
 
 
