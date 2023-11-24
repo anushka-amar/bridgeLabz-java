e16ffec4b8e2cf1eba593b16bde093d6c378b887
Anushka <anushkamar1102@gmail.com>
2023-11-24 14:05:46 +0530

    Refactored: created an object  and initialised its position

---
+++ src/snakeladdergame/Player.java
@@ -0,0 +1,19 @@
+package snakeladdergame;
+
+public class Player {
+
+    private int currPos;
+
+    public Player(){
+        this.currPos = 0;
+    }
+
+    public int getCurrPos(){
+        return currPos;
+    }
+
+    public void setCurrPos(int currPos){
+        this.currPos = currPos;
+    }
+
+}
--- src/snakeladdergame/SnakeAndLadder.java
+++ src/snakeladdergame/SnakeAndLadder.java
@@ -3,5 +3,8 @@ package snakeladdergame;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to the game");
+
+        Player player1 = new Player();
+        System.out.println("Player1 your current position is: "+ player1.getCurrPos());
     }
 }
